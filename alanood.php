73c39b71e9c3208e1575c7ead28a8aebcece8d3c
Alanoodabdullah <156507909+Alanoodabdullah@users.noreply.github.com>
2024-05-31 19:22:46 +0300

    Add files via upload

---
+++ alanood.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= <h1>Hello</h1>
+    <p>my name is alanood abdullah</p>
+
+    <?php
+    each "<h1>alanood</h>";
+    print "<h1>alanood</h1>";
+    ?>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
